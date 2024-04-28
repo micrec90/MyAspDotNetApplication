9efaba76b18ba20efaa05f5dc70a3b3cfbcf61bf
Michal Reclaw <micrec90@gmail.com>
2024-04-28 22:59:35 +0200

    Created User and ForumPost models

---
+++ MyAspDotNetApplication/MyRESTfulWebAPI/Models/ForumPost.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRESTfulWebAPI.Models
+{
+    public class ForumPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ MyAspDotNetApplication/MyRESTfulWebAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRESTfulWebAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime RegisteredOn { get; set; } = DateTime.Now;
+        public List<ForumPost> ForumPosts { get; set; } = new List<ForumPost>();
+    }
+}
