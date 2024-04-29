934c66736c5b51acfa76a1f389f852c52001fd1e
Michal Reclaw <micrec90@gmail.com>
2024-04-30 00:51:32 +0200

    Controllers

---
+++ MyAspDotNetApplication/MyRESTfulWebAPI/Controllers/ForumPostsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyRESTfulWebAPI.Context;
+using MyRESTfulWebAPI.Models;
+
+namespace MyRESTfulWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumPostsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ForumPostsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ForumPosts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ForumPost>>> GetForumPosts()
+        {
+            return await _context.ForumPosts.ToListAsync();
+        }
+
+        // GET: api/ForumPosts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ForumPost>> GetForumPost(int id)
+        {
+            var forumPost = await _context.ForumPosts.FindAsync(id);
+
+            if (forumPost == null)
+            {
+                return NotFound();
+            }
+
+            return forumPost;
+        }
+
+        // PUT: api/ForumPosts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutForumPost(int id, ForumPost forumPost)
+        {
+            if (id != forumPost.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(forumPost).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ForumPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ForumPosts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ForumPost>> PostForumPost(ForumPost forumPost)
+        {
+            _context.ForumPosts.Add(forumPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetForumPost", new { id = forumPost.Id }, forumPost);
+        }
+
+        // DELETE: api/ForumPosts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteForumPost(int id)
+        {
+            var forumPost = await _context.ForumPosts.FindAsync(id);
+            if (forumPost == null)
+            {
+                return NotFound();
+            }
+
+            _context.ForumPosts.Remove(forumPost);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ForumPostExists(int id)
+        {
+            return _context.ForumPosts.Any(e => e.Id == id);
+        }
+    }
+}
